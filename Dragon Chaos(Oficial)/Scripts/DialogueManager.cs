338c6beac984c25154e7a9d466dae491ad385ff2
McKinley-TheProProgrammer <54154558+McKinley-TheProProgrammer@users.noreply.github.com>
2019-08-20 14:43:14 -0300

    Add files via upload

---
+++ Dragon Chaos(Oficial)/Scripts/DialogueManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    private Queue<string> sentencas;
+	// Use this for initialization
+	void Start () {
+        sentencas = new Queue<string>();
+	}
+	
+	public void ComecarDialogo(Dialogue dialogo)
+    {
+        sentencas.Clear();
+
+        foreach(string frase in dialogo.sentencas)
+        {
+            sentencas.Enqueue(frase);
+        }
+
+        DisplayNextFrase();
+    }
+    public void DisplayNextFrase()
+    {
+        if(sentencas.Count == 0)
+        {
+            EndDialogo();
+            return;
+        }
+        string frase = sentencas.Dequeue();
+    }
+    public void EndDialogo()
+    {
+        Debug.Log("Fim da Conversa");
+    }
+}
---
+++ Dragon Chaos(Oficial)/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float velocity = 5f;
+    float jumpForce = 300f;
+    Rigidbody2D body;
+    Animator anim;
+    SpriteRenderer flipa;
+    // Use this for initialization
+    public bool isActive = true;
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        isActive = true;
+        anim = GetComponent<Animator>();
+        flipa = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(x * velocity,body.velocity.y);
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            body.AddForce(new Vector2(0, jumpForce));
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            flipa.flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            flipa.flipX = false;
+        }
+        
+        if (x == 0)
+        {
+            anim.SetBool("IsWalking",false);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        if(x == 1 || x == -1)
+        {
+            anim.SetTrigger("StartWalking");
+        }
+        else
+        {
+            anim.SetTrigger("EndWalking");
+        }
+       
+    }
+}
